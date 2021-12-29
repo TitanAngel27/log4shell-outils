81ae96940f27aa5123d8641ce986d0c385748aa8
Philippe Tanguay <phtan7@ulaval.ca>
2021-12-29 16:48:09 -0500

    Supportesr le telechargement des 27 listes

--- README.md
+++ README.md
@@ -11,14 +11,17 @@ pip install -r requirements.txt
 ## Utilisation
 **softwarelist_parser.py**
 ```bash
-py softwarelist_parser.py csv
+py softwarelist_parser.py #Windows
+python softwarelist_parser.py #Linux
 ```
 Description: \
-Télécharge la dernière liste publiée puis met le tout en format csv dans le dossier Liste sous le format _list_log4shell_mm-jj_.
+Télécharge les 27 dernières listes publiées puis met le tout en format csv dans le dossier Liste sous le format _list_log4shell_mm-jj_. \
+À chaque fois que l'on utilise la commande, on laisse une trace dans le fichier _liste.txt_.
 
 **softwarelist_compare.py**
 ```bash
-py softwarelist_compare.py
+py softwarelist_compare.py #Windows
+python softwarelist_compare.py #Linux
 ```
 Description: \
 Compare la liste du jour et la liste de la journée d'avant, puis créer un fichier texte dans le dossier Resultat sous le format _Resultat_mm-jj_. Pour l'instant les fichiers donnent plus d'informations que nécessaire (À changer et/ou automatiser). Les parties importantes sont : \
--- softwarelist_parser.py
+++ softwarelist_parser.py
@@ -1,12 +1,12 @@
 import csv as py_csv
-import sys
-import json as py_json
-import pathlib
-import tempfile
+#import sys
+#import json as py_json
+#import pathlib
+#import tempfile
 
 from typing import List
 
-import click
+#import click
 import mistune
 import requests
 import unicodedata
@@ -25,15 +25,44 @@ HEADERS = [
     'Status CVE-2021-45046',
     'Status CVE-2021-45105',
     'Notes',
-    'Links'
+    'Links',
 ]
 
-def download_softwarelist() -> str:
-    """ Download softwarelist to parse
+LIENS_LIST = [
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_0-9.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_a.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_b.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_c.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_d.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_e.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_f.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_g.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_h.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_i.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_j.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_k.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_l.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_m.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_n.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_o.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_p.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_q.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_r.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_s.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_t.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_u.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_v.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_w.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_x.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_y.md',
+    'https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/software_list_z.md',
+]
 
+def download_softwarelist(url: str) -> str:
+    """ Download softwarelist to parse
+    :param: le lien du fichier raw github
     :return: Markdown content of software list
     """
-    url = "https://raw.githubusercontent.com/NCSC-NL/log4shell/main/software/README.md"
     response = requests.get(url)
     return response.content
 
@@ -66,37 +95,75 @@ def parse_record(record: List[Tag] = None) -> dict:
 
     return result
 
-@click.group()
+def update_txt(current_date: date):
+    cheminListe = '.\Liste\liste.txt'
+    f = open(cheminListe, 'a',encoding='utf-8')
+    f.write("Update: "+str(current_date.month)+"-"+str(current_date.day)+'\n')
+    f.close
+
+#@click.group()
 #@click.option('--path', default='./README.md', help='Path to software list README.md', type=click.File(encoding='UTF-8'))#Specifier Encoding
-@click.pass_context
-def main(ctx):
-
-    # Parse Markdown to HTML and get soup
-    #content = path.read()
-    content = download_softwarelist()
-    html = mistune.html(content.decode('utf-8'))
-    soup = BeautifulSoup(html, 'html.parser')
-
-    # Look for all tr columns with td fields, after first h3 header
+#@click.pass_context
+def main():
+    obj = {}
     records = list()
-    first_h3 = soup.find('h3')
 
-    for x in first_h3.next_elements:
-        if isinstance(x, Tag) and x.name == 'tr':
-            tds = x.find_all('td')
+    for url in LIENS_LIST:
+        # Parse Markdown to HTML and get soup
+        #content = path.read()
+        content = download_softwarelist(url)
+        html = mistune.html(content.decode('utf-8'))
+        soup = BeautifulSoup(html, 'html.parser')
 
-            if tds:  # ensure empty tr are ignored
-                records.append(parse_record(tds))
+        # Look for all tr columns with td fields, after first h3 header
+        
+        first_h3 = soup.find('h3')
 
-    ctx.obj['records'] = records
+        for x in first_h3.next_elements:
+            if isinstance(x, Tag) and x.name == 'tr':
+                tds = x.find_all('td')
+
+                if tds:  # ensure empty tr are ignored
+                    records.append(parse_record(tds))
+
+    obj['records'] = records
+
+    #Déplacer l'option CSV dans le main
+
+    HEADERS.append('Date MAJ')
+
+    current_day = date.today()
+
+    nomcsv = '.\Liste\list_log4shell_'+str(current_day.month)+'-'+str(current_day.day)+'.csv'
+
+    with open(nomcsv, 'w+', encoding='UTF-8', newline='') as csvfile:
+        writer = py_csv.DictWriter(csvfile, HEADERS, delimiter=';')
+
+        writer.writeheader()
+
+        # cleanup links
+        for record in obj['records']:
+            record['Links'] = list(record['Links'].values())
+
+            #ADD date record
+            
+            record['Date MAJ'] = date.today()
+
+            writer.writerow(record)
+
+    update_txt(current_day)
 
 
+
+'''
 @main.command()
 @click.argument('output', default='-', type=click.File('w+'))
 @click.pass_context
 def json(ctx, output):
     py_json.dump(ctx.obj['records'], output)
+'''
 
+'''
 @main.command()
 #@click.argument('output', default='list.csv', type=click.File('w+',encoding='UTF-8'))
 @click.pass_context
@@ -118,10 +185,10 @@ def csv(ctx):#, output):
             record['Links'] = list(record['Links'].values())
 
             #ADD date record
-            #record['Date MAJ'] = date.today()
+            record['Date MAJ'] = date.today()
 
             writer.writerow(record)
-
+'''
 
 if __name__ == '__main__':
-    main(obj={})
+    main()
